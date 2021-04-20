9a794236c208f0d59b3dfb33fe2639900cb6b75d
Emre Erkoca <emreerkoca@outlook.com>
2021-04-20 17:17:49 +0300

    TransactionScopeSample added

---
+++ ArticleSamples/TransactionScopeSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Transactions;
+
+namespace TransactionScopeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TransactionScope
+        }
+    }
+}
