7831dc452bc49b280f2e5260c2876e6a62e9a64e
Emre Erkoca <emreerkoca@outlook.com>
2021-04-23 14:07:04 +0300

    Transaction Scope sample completed

--- ArticleSamples/TransactionScopeSample/Program.cs
+++ ArticleSamples/TransactionScopeSample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Transactions;
 
 namespace TransactionScopeSample
@@ -7,7 +8,37 @@ namespace TransactionScopeSample
     {
         static void Main(string[] args)
         {
-            //TransactionScope
+            try
+            {
+                using (TransactionScope scope = new TransactionScope())
+                {
+                    string connectionString = "YourConnectionString";
+
+                    using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                    {
+                        sqlConnection.Open();
+
+                        string updatPersonPhoneQuery = "Update [AdventureWorks2012].[Person].[PersonPhone] SET PhoneNumber = '697-555-0145' WHERE BusinessEntityID = 1";
+                        SqlCommand updatePersonPhoneSqlCommand = new SqlCommand(updatPersonPhoneQuery, sqlConnection);
+
+                        updatePersonPhoneSqlCommand.ExecuteNonQuery();
+
+                        //throw new TransactionAbortedException("error");
+
+                        string updatePersonQuery = $"UPDATE [AdventureWorks2012].[Person].[Person] SET ModifiedDate = '{DateTime.UtcNow}' WHERE BusinessEntityID = 1";
+                        SqlCommand updatePersonSqlCommand = new SqlCommand(updatePersonQuery, sqlConnection);
+
+                        updatePersonSqlCommand.ExecuteNonQuery();
+                        sqlConnection.Close();
+                    }
+
+                    scope.Complete();
+                }
+            }
+            catch (TransactionAbortedException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
