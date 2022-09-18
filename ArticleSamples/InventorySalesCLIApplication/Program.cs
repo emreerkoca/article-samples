7b99a77ba0c4776d3aebc32dfe2c0a2a06816d5b
Emre Erkoca <emreerkoca@outlook.com>
2022-09-18 13:16:59 +0300

    CLI app added. System.CommandLine beta library used

---
+++ ArticleSamples/InventorySalesCLIApplication/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.CommandLine;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventorySalesCLIApplication
+{
+    public class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var fileOption = new Option<FileInfo?>(
+                name: "--file",
+                description: "The file to read and display on the console.");
+
+            var rootCommand = new RootCommand("Sample app for System.CommandLine");
+            rootCommand.AddOption(fileOption);
+
+            rootCommand.SetHandler((file) =>
+            {
+                ReadFile(file!);
+            },
+                fileOption);
+
+            return await rootCommand.InvokeAsync(args);
+        }
+
+        static void ReadFile(FileInfo file)
+        {
+            File.ReadLines(file.FullName).ToList()
+                .ForEach(line => Console.WriteLine(line));
+        }
+    }
+}
